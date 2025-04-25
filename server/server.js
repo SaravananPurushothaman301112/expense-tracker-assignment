3334c3132b9f1618d08de5b1a0c04b3187332127
SaravananPurushothaman301112 <saravananraj3011@gmail.com>
2025-04-26 02:20:30 +0530

    CORS configuration for allowing only MY CLIENT

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',  // Replace with the specific origin if needed
+}));
 app.use(express.json());
 
 // Database connection
